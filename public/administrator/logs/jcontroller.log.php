b7cfab31d4f3bcc3543d60d26a2271f2e5cb7c27
Dianne <cookiedaktulo@gmail.com>
2022-10-29 19:34:33 +0100

    first commit

---
+++ public/administrator/logs/jcontroller.log.php
@@ -0,0 +1,26 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-10-29 18:26:35 UTC
+#Software: Joomla! 4.2.4 Stable [ Uaminifu ] 25-October-2022 15:20 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-10-29T18:26:35+00:00	INFO 192.168.0.39	controller	Holding edit ID com_menus.edit.menu.2 Array (     [0] => 2 ) 
+2022-10-29T18:26:36+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_menus.edit.menu.2 Array ( ) 
+2022-10-29T18:27:38+00:00	INFO 192.168.0.39	controller	Holding edit ID com_categories.edit.category.2 Array (     [0] => 2 ) 
+2022-10-29T18:27:38+00:00	INFO 192.168.0.39	controller	Checking edit ID com_categories.edit.category.2: 1 Array (     [0] => 2 ) 
+2022-10-29T18:27:41+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_categories.edit.category.2 Array ( ) 
+2022-10-29T18:27:59+00:00	INFO 192.168.0.39	controller	Holding edit ID com_modules.edit.module.1 Array (     [0] => 1 ) 
+2022-10-29T18:27:59+00:00	INFO 192.168.0.39	controller	Checking edit ID com_modules.edit.module.1: 1 Array (     [0] => 1 ) 
+2022-10-29T18:28:02+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_modules.edit.module.1 Array ( ) 
+2022-10-29T18:28:04+00:00	INFO 192.168.0.39	controller	Holding edit ID com_modules.edit.module.16 Array (     [0] => 16 ) 
+2022-10-29T18:28:05+00:00	INFO 192.168.0.39	controller	Checking edit ID com_modules.edit.module.16: 1 Array (     [0] => 16 ) 
+2022-10-29T18:28:11+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_modules.edit.module.16 Array ( ) 
+2022-10-29T18:28:20+00:00	INFO 192.168.0.39	controller	Holding edit ID com_modules.edit.module.1 Array (     [0] => 1 ) 
+2022-10-29T18:28:20+00:00	INFO 192.168.0.39	controller	Checking edit ID com_modules.edit.module.1: 1 Array (     [0] => 1 ) 
+2022-10-29T18:28:32+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_modules.edit.module.1 Array ( ) 
+2022-10-29T18:28:39+00:00	INFO 192.168.0.39	controller	Holding edit ID com_modules.edit.module.1 Array (     [0] => 1 ) 
+2022-10-29T18:28:39+00:00	INFO 192.168.0.39	controller	Checking edit ID com_modules.edit.module.1: 1 Array (     [0] => 1 ) 
+2022-10-29T18:28:49+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_modules.edit.module.1 Array ( ) 
+2022-10-29T18:28:55+00:00	INFO 192.168.0.39	controller	Holding edit ID com_modules.edit.module.1 Array (     [0] => 1 ) 
+2022-10-29T18:28:55+00:00	INFO 192.168.0.39	controller	Checking edit ID com_modules.edit.module.1: 1 Array (     [0] => 1 ) 
+2022-10-29T18:29:40+00:00	INFO 192.168.0.39	controller	Releasing edit ID com_modules.edit.module.1 Array ( ) 
